5f9656aeda9b5a3bfd33f6e03cc1bfa9cb35b5db
Dorian-Knight <34255884+DorianKnight@users.noreply.github.com>
2024-07-26 19:02:58 -0400

    Create compare_emg_input_with_sequence.ino

---
+++ compare_emg_input_with_sequence/compare_emg_input_with_sequence.ino
@@ -0,0 +1,18 @@
+String example_user_input = "0312";
+String example_sequence = "0312";
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  bool user_correct = compare_user_to_sequence(example_user_input, example_sequence);
+  Serial.print("User is correct: ");
+  Serial.println(user_correct);
+}
+
+bool compare_user_to_sequence(String user_input, String sequence){
+  return user_input == sequence;
+}
