2d0683276e2fa458ebd88091908eb4bd350bf5e0
Dorian-Knight <34255884+DorianKnight@users.noreply.github.com>
2024-07-28 18:30:32 -0400

    First functional EMG mainloop with all individual atomized functions included

--- EMG_Mainloop/EMG_Mainloop.ino
+++ EMG_Mainloop/EMG_Mainloop.ino
@@ -3,24 +3,29 @@
 // Description: Main function to run EMG Simon Says
 
 /* Global Defs */
+/* output_sequence */
+
 // Legend
 // Left bicep: 1
 // Right bicep: 2
 // Left calf: 3
 // Right calf: 4
-const int left_bicep_active = 1;
-const int right_bicep_active = 2;
-const int left_calf_active = 3;
-const int right_calf_active = 4;
+const char left_bicep_active  = '0';
+const char right_bicep_active = '1';
+const char left_calf_active   = '2';
+const char right_calf_active  = '3';
 
-/* output_sequence */
-int left_bicep_led = PD2;
-int right_bicep_led = PD3;
-int left_calf_led = PD4;
-int right_calf_led = PD5;
-int desired_led;
+int left_bicep_led    = PD2;
+int right_bicep_led   = PD3;
+int left_calf_led     = PD4;
+int right_calf_led    = PD5;
+char desired_led;
 int array_len = 4;
-//int sequence_array[4] = {0,3,1,2};
+String sequence_array = "0312";
+
+int output_wrong = 0;
+int output_right = 1;
+int output_pattern = 2;
 
 /* read_emg_input */
 float left_bicep_emg_raw;
@@ -28,12 +33,12 @@ float right_bicep_emg_raw;
 float left_calf_emg_raw;
 float right_calf_emg_raw;
 
-int left_bicep_input_pin = A0;
+int left_bicep_input_pin  = A0;
 int right_bicep_input_pin = A1;
-int left_calf_input_pin = A2;
-int right_calf_input_pin = A3;
+int left_calf_input_pin   = A2;
+int right_calf_input_pin  = A3;
 
-float threshold = 400;  // Tune to specific situation
+float threshold = 600;  // Tune to specific situation
 const int total_samples = 50;
 
 void setup() {
@@ -50,29 +55,48 @@ void setup() {
   /* read_emg_input */
 }
 
+bool response_check = true;
+String prev_sequence = "";
+String sequence;
+String user_input_sequence;
+
 void loop() {
-  // put your main code here, to run repeatedly:
-  Serial.println("Reading Muscle 1");
-  int active_muscle1 = read_emg_raw();
-  delay(1000);
-  Serial.println("Reading Muscle 2");
-  int active_muscle2 = read_emg_raw();
-  delay(1000);
-  Serial.println("Reading Muscle 3");
-  int active_muscle3 = read_emg_raw();
-  delay(1000);
-  Serial.println("Reading Muscle 4");
-  int active_muscle4 = read_emg_raw();
-  delay(1000);
+  // Generate a sequence to output
+ sequence = generate_string_sequence(prev_sequence, response_check);
+
+  // Output the sequence
+  output_led_sequence(sequence, output_pattern);
+
+  // Read user input
+  user_input_sequence = "";
+  for (int i=0; i<sequence.length(); i++) {
+    Serial.println("Reading Muscle " + (String)i);
+    char user_input = read_emg_raw();
+    user_input_sequence = user_input_sequence + read_emg_raw();
+
+    // Check if user input matches
+    response_check = compare_user_to_sequence(user_input_sequence, sequence, user_input_sequence.length());
+    //Serial.println("Response check: " + (String) response_check);
+    if (!response_check) {
+      break;
+    }
+  }
   
-  int mode = 2;
-  int sequence_array[4] = {active_muscle1,active_muscle2,active_muscle3,active_muscle4};
-  output_led_sequence(sequence_array, sizeof(sequence_array)/sizeof(int), mode);
+
+  if (response_check == false) {
+    // Tell the user that they're wrong
+    output_led_sequence(sequence, output_wrong);
+  }
+
+  else {
+    output_led_sequence(sequence, output_right);
+  }
+
+  prev_sequence = sequence;
 }
 
 
-
-void output_led_sequence(int sequence_array[], int array_len, int mode) {
+void output_led_sequence(String sequence_array, int mode) {
   // Mode 0: Indicate that the user was correct in their input sequence
   // Mode 1: Indicate that the user was incorrect in their input sequence
   // Other: Output the randomized LED sequence sent from main function
@@ -82,12 +106,12 @@ void output_led_sequence(int sequence_array[], int array_len, int mode) {
   }
 
   else if (mode == 1) {
-    output_result(right_bicep_led);
+    output_result(left_calf_led);
   }
 
   else {
-    for (int i=0; i<array_len; i++){
-      desired_led = sequence_array[i];
+    for (int i=0; i<sequence_array.length(); i++){
+      desired_led = sequence_array.charAt(i);
       switch (desired_led) {
         case left_bicep_active:  // Left bicep
           illuminate_led(left_bicep_led);
@@ -102,11 +126,13 @@ void output_led_sequence(int sequence_array[], int array_len, int mode) {
           illuminate_led(right_calf_led);
           break;
         default:
-          Serial.println("Default :)");
+          Serial.print("Default: desired LED: ");
+          Serial.println(desired_led);
       }
     }
   }
 }
+
 
 void illuminate_led(int led_pin){
   digitalWrite(led_pin, HIGH);
@@ -124,9 +150,10 @@ void output_result(int led_pin){
   }
 }
 
-int read_emg_raw() {
-  int active_muscle = 0;
-  while (active_muscle == 0){
+char read_emg_raw() {
+  bool muscle_acquired = false;
+  char active_muscle;
+  while (!muscle_acquired){
     // Serial.println("Reading emg inputs");
     // Initialize empty list for raw emg samples
     float emg_vals_left_bicep[total_samples];
@@ -174,20 +201,49 @@ int read_emg_raw() {
     // Find which muscle was active above threshold
     if (emg_left_bicep_avg >= threshold){
       active_muscle = left_bicep_active;
+      muscle_acquired = true;
     }
     else if(emg_right_bicep_avg >= threshold){
       active_muscle = right_bicep_active;
+      muscle_acquired = true;
     }
     else if(emg_left_calf_avg >= threshold){
       active_muscle = left_calf_active;
+      muscle_acquired = true;
     }
     else if (emg_right_calf_avg >= threshold){
       active_muscle = right_calf_active;
-    }
-    else{
-      // No muscle is active
-      active_muscle = 0;
+      muscle_acquired = true;
     }
   }
+  delay(1000);
   return active_muscle;
+}
+
+bool compare_user_to_sequence(String user_input, String sequence, int stop_compare_index){
+  bool match_held = true;
+  for (int i=0; i<stop_compare_index; i++) {
+    match_held = user_input.charAt(i) == sequence.charAt(i);
+    if (!match_held) {
+      break;
+    }
+  }
+  return match_held;
+}
+
+String generate_string_sequence(String prev_sequence, bool response_check) {
+  // If the user is wrong, reset sequence
+  String new_sequence;
+  if (response_check == false) {
+    new_sequence = (String)random(4);
+  }
+
+  // If the user passes previous level, concatenate new string with previous string
+  else {
+    String new_number = (String)random(4);
+    new_sequence = prev_sequence + new_number; 
+  }
+
+  return new_sequence;
+
 }
